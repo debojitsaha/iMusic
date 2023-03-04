f5ec36476ec8978e8b677782881c424cafe1f1ac
debojit <saha05debojit@gmail.com>
2023-03-04 20:17:17 +0530

    initial setup

---
+++ src/components/MusicPlayer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./MusicPlayer.scss";
+
+const Music_Player = () => {
+
+  return (
+    <div className="music">
+      Music Player
+    </div>
+  );
+};
+
+export default Music_Player;
