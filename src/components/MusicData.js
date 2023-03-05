6c0cd66137b07300320db95b205eb2d633326579
debojit <saha05debojit@gmail.com>
2023-03-06 00:41:29 +0530

    music data expanded

--- src/components/MusicData.js
+++ src/components/MusicData.js
@@ -1,11 +1,16 @@
 import artist1 from "../assets/artist1.webp";
 import artist2 from "../assets/artist2.webp";
+import artist3 from "../assets/artist3.webp";
 import thumbnail1 from "../assets/thumbnail1.webp";
 import thumbnail2 from "../assets/thumbnail2.webp";
 import thumbnail3 from "../assets/thumbnail3.webp";
+import thumbnail4 from "../assets/thumbnail4.webp";
+import thumbnail5 from "../assets/thumbnail5.webp";
 import song1 from '../assets/Songs/Bhalobashar Morshum (Male version)_320- [PagalWorld.NL].mp3'
 import song2 from '../assets/Songs/Ilahi.mp3'
 import song3 from '../assets/Songs/Saari Ki Saari 2.0.mp3'
+import song4 from '../assets/Songs/10 - Gulabi Ankhen (Darshan Raval) - Indias Raw Star.mp3'
+import song5 from '../assets/Songs/06 Nazm Nazm Ft Ayushmann Khurrana 190Kbps.mp3'
 
 const data = [
   {
@@ -28,6 +33,20 @@ const data = [
     artist: "Darshan Raval",
     thumbnail: thumbnail3,
     src: song3
+  },
+  {
+    profile: artist2,
+    name: "Gulabi Ankhen",
+    artist: "Darshan Raval",
+    thumbnail: thumbnail4,
+    src: song4
+  },
+  {
+    profile: artist3,
+    name: "Nazm Nazm",
+    artist: "Ayushmann Khurrana",
+    thumbnail: thumbnail5,
+    src: song5
   }
 ];
 
